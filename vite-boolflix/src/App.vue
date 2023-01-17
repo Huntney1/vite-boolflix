6718bcb8b861b98be98ebb72f02d72478c14845c
Huntney1 <devilwhiteone@hotmail.it>
2023-01-18 00:58:34 +0100

    inizio

---
+++ vite-boolflix/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
+import axios from "axios";
+import { store } from "./store.js";
+
+export default{
+  components:{
+    AppHeader,
+    AppMain,
+  },
+
+  data() {
+    return {
+      store
+  }
+},
+}
+
+</script>
+
+<template lang="">
+  <AppHeader />
+  <AppMain />
+    
+</template>
+
+<style lang="scss">
+
+@use "./components/style/generals.scss" as *;
+
+</style>
---
+++ vite-boolflix/src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+
+
+export default {
+    components:{
+        
+    }
+}
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
