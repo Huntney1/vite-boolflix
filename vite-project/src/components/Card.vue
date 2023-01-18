502739b57768b0ce121491eaa788b0242a2d2e54
Huntney1 <devilwhiteone@hotmail.it>
2023-01-18 11:46:07 +0100

    card

--- vite-project/src/components/Card.vue
+++ vite-project/src/components/Card.vue
@@ -1,28 +1,32 @@
 <script>
 export default {
-  props:{
-    movie: Object,
-  }  
+    props: {
+        movie: Object,
+    },
+    data() {
+        return {
+            index: 0,
+        }
+    },
 };
 </script>
 
 <template lang="">
-     <div class="card">
-    <h4>Titolo:"{{ movie.title }}"</h4>
-    <h5>Titolo originale:"{{ movie.original_title }}"</h5>
-    <div>
-      <p>lingua:"{{ movie.original_language }}"</p>
-      <p>voto:"{{ movie.vote_average }}"</p>
+    <div class="card">
+        <img class="img-fluid" :src="movie.poster_pat"></img>
+        <h4>Titolo:"{{ movie.title }}"</h4>
+            <h5>Titolo originale:"{{ movie.original_title }}"</h5>
+        <div>
+            <p>lingua:"{{ movie.original_language }}"</p>
+            <p>voto:"{{ movie.vote_average }}"</p>
+        </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
-    
-.cards{
+.cards {
     border: 1px solid black;
     padding: 10px;
     margin: 10px;
 }
-
 </style>
--- vite-project/src/store.js
+++ vite-project/src/store.js
@@ -2,10 +2,9 @@ import { reactive } from 'vue'
 
 export const store = reactive({
     apiKey: 'e7fcd33c417cee689daaf756efeb4299',
-    url: 'https://api.themoviedb.org/3/movie/550?api_key=e7fcd33c417cee689daaf756efeb4299&language=en-US&query=',
-   
+    url: 'https://api.themoviedb.org/3/search/movie?api_key=e7fcd33c417cee689daaf756efeb4299&language=en-US&query=',
     moviesArr: [],
-    selectMovie: 'Fight Club',
+    selectMovie: 'Harry Potter',
 
 
 });
