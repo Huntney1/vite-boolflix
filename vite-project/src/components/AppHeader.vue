c98266aadf44d5028bdc6d38e443061a5e579962
Huntney1 <devilwhiteone@hotmail.it>
2023-01-18 02:06:13 +0100

    inser dati preparazione ho dovuto cancellare tutto

---
+++ vite-project/src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+
+}
+
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../components/style/partials/variables' as *;
+@use '../components/style/partials/mixins' as *;
+
+</style>
---
+++ vite-project/src/store.js
@@ -0,0 +1,10 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+    apiKey: 'e7fcd33c417cee689daaf756efeb4299',
+    url: 'https://api.themoviedb.org/3/movie/550?api_key=e7fcd33c417cee689daaf756efeb4299&language=en-US&query=',
+    flagApi: 'https://countryflagsapi.com/png/ae',
+    muvieArr: [],
+
+
+});
